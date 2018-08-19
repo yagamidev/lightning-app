fdc500f05853a0700237977e016af328514a3536
Valentine Wallace <wallaceness5@gmail.com>
2018-08-19 14:41:47 -0700

    Add view for new password entry during reset password flow.

--- stories/screen.js
+++ stories/screen.js
@@ -47,6 +47,7 @@ import SeedVerify from '../src/view/seed-verify';
 import SetPassword from '../src/view/set-password';
 import Password from '../src/view/password';
 import ResetPasswordCurrent from '../src/view/reset-password-current';
+import ResetPasswordNew from '../src/view/reset-password-new';
 import NewAddress from '../src/view/new-address';
 import Wait from '../src/view/wait';
 import RestoreWallet from '../src/view/restore-wallet';
@@ -101,6 +102,9 @@ storiesOf('Screens', module)
   .add('Password', () => <Password store={store} wallet={wallet} />)
   .add('Reset Password - Current', () => (
     <ResetPasswordCurrent store={store} wallet={wallet} nav={nav} />
+  ))
+  .add('Reset Password - New', () => (
+    <ResetPasswordNew store={store} wallet={wallet} nav={nav} />
   ))
   .add('New Address', () => (
     <NewAddress store={store} invoice={invoice} info={info} />
